7ab63756db92d29ba4f6711d88c0f4f49b832cff
Joao Batista Monteiro <jbmjbmjbm@hotmail.com>
2023-09-08 19:15:36 -0300

    01

---
+++ aula.js
@@ -0,0 +1,86 @@
+// comando print
+console.log('João Monteiro')
+console.log('Bemv vindo a aula')
+
+//camelCase
+// definindo variaveis
+let idade = 49;
+console.log(idade);
+
+let altura = 180;
+console.log(altura);
+
+let peso = 79;
+console.log(peso);
+
+
+// contantes
+//const valorIngressoAdulto = 55;
+let valorIngressoAdulto = 55;
+valorIngressoAdulto = 80;
+console.log(valorIngressoAdulto);
+
+//----------------------------------------------
+
+//Tipos de dados primitivos
+let nome = 'Joao Monteiro$$000'; //string lteral
+console.log(nome);
+
+let idade1 = 35.05; // number literal
+console.log(idade1);
+
+let estaAprovado = true; // boolean
+console.log(estaAprovado);
+
+let sobrenome; // undefine; // Undefine
+console.log(sobrenome);
+
+let corSelecionada = null;
+console.log(corSelecionada); // redefinir um valor
+
+//----------------------------------------------
+
+//Tipagem dinamica
+// typeof comando para verificar o tipo 
+//e poder alterar com variavel = valor
+
+// typeof corSelecionada
+'object'
+corSelecionada = 10
+10
+typeof corSelecionada
+'number'//
+
+//----------------------------------------------
+
+let pessoa ={
+    nome:'Joao Monteiro',
+    idade: 25,
+    estaAprovado: true,
+    corSelecionada: null
+
+};
+console.log(pessoa);
+
+//----------------------------------------------
+// parada 28,24 minutos
+
+//----------------------------------------------
+ //Arrays
+
+ let familia = ['João','Sandra','Hnerique','Helena'];
+ console.log(familia.length);
+ console.log(familia[3]);
+
+ let nomeColega = [];
+
+ //----------------------------------------------
+ //parada 32,40 minutos
+ //  Função
+ // nomear uma função verbo + subtantivo
+
+
+
+
+
+ 
